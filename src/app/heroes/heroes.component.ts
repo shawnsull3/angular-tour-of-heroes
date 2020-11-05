c89a0468689f891c5d4d3c1d7b74e054ff7d685f
Shawn Sullivan <sullivansm3@gmail.com>
2020-11-05 10:42:51 -0700

    Allow user to click on hero to get details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,17 @@ import { HEROES } from '../mock-heros';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
-
   heroes = HEROES;
+  selectedHero: Hero;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
